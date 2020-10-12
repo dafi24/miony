dc1e9b1dcbbb62e6bbb97f89d5bf29b0f65f4b4e
davidmiony <57227109+davidmiony@users.noreply.github.com>
2020-10-12 13:20:27 +0200

    uprava

---
+++ mereni.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 16 11:44:46 2020
+
+@author: David
+"""
+import vyhodnoceni as vhd
+import numpy as np
+import matplotlib.pyplot as plt
+import os.path as op
+from nabiraniA import naberDataA
+
+# 1 SAMPLE = (12/245) us              
+
+cislo = 1    
+while(True):
+    
+    cas,napeti=naberDataA()
+    napeti = -napeti
+    
+    index1, index1a, index2, index2a = vhd.vyhodnoceni(cas,napeti)
+            
+    proc=0.3
+    
+    cas1 = vhd.caspulsu(cas,napeti,index1,index1a,proc)    
+    cas2 = vhd.caspulsu(cas,napeti,index2,index2a,proc)
+    
+    ROZDIL=(cas2-cas1)/1e3
+    print("Časová vzdálenost pulsů: {:.2f} us".format(ROZDIL))
+    print("čas pulsu 1: {:.2f} ns".format(cas1))
+    print("čas pulsu 2: {:.2f} ns".format(cas2))
+        
+    if(cas1<0):
+        print("Nabiraní vrátilo data bez žádného pulsu")
+    elif(cas2<0):                 
+        print("Čas 1 [ms]:",cas1)        
+#        plt.figure(1)
+#        plt.plot(cas[index1-30:index1+30],napeti[index1-30:index1+30])
+#        plt.show()
+    else:
+        print("Čas 1 [ms]:",cas1)
+        print("Čas 2 [ms]:",cas2)
+        print("Rozdíl [ms]:",cas2-cas1)
+        np.savetxt("rozpad {}.txt".format(cislo),(cas,napeti),delimiter=",")
+        cislo+=1
+#        plt.figure(2)
+#        plt.plot(cas[index1-30:index2a+30],napeti[index1-30:index2a+30])
+#        plt.show()
---
+++ novevyhodnoceni.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  4 15:59:43 2020
+
+@author: David
+"""
+
+import numpy as np
+def caspulsu(cas,napeti,indexZ,indexK,proc):
+    puls = napeti[indexZ:indexK]
+    if(len(puls)==0 or indexZ >= 2449):
+        cas = -9999e6
+    else:
+        amplitudaindex = np.where(puls==np.amax(puls))[0][0] + indexZ
+        nap = proc*napeti[amplitudaindex]
+        ind = 0
+        for x in napeti[indexZ:indexK]:
+            ind += 1
+            if(x>nap):
+                break
+        U1 = napeti[amplitudaindex-1]
+        U2 = napeti[amplitudaindex]
+        t1 = cas[amplitudaindex-1]
+        t2 = cas[amplitudaindex]
+        a = (U1-U2)/(t1-t2)
+        b = U1-(a*t1)
+        cas = (nap-b)/a
+    return cas
+
+def vyhodnoceni(cas,napeti):
+    
+    trigger = 50
+    reset = 20
+    sampleTime = 12*1e3/245
+    
+    index1=0
+    for x in napeti:
+        if(abs(x)>trigger):
+            break
+        index1+=1
+    
+    index1a=index1
+    for x in napeti[index1:]:
+        index1a+=1
+        if(abs(x)<reset):
+            break
+
+# index1:index1a = kladná (resp. záporná) část prvního pulsu
+# index1 - jeden prvek před překročení triggeru
+# index1a - reset (první prvek menší než 20)
+    
+    index2=index1a
+    for x in napeti[index1a:]:
+        if((abs(x)>trigger) and ((abs(x)-abs(napeti[index2]))/sampleTime>1)):
+            break
+        index2+=1
+    
+    index2a=index2
+    for x in napeti[index2:]:
+        index2a+=1
+        if(abs(x)<reset):
+            break
+# index2a bude vždy minimálně o 1 větší, než index2, což může být problém, pokud index2 je poslední sample z napětí (až se tohle vyřeší, teprve potom bude fungovat casPulsu)    
+    casPulsu1 = caspulsu(cas,napeti,index1,index1a,0.3)/1e6
+    casPulsu2 = caspulsu(cas,napeti,index2,index2a,0.3)/1e6
+    return casPulsu1,casPulsu2,index1,index2a
