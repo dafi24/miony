c565d0b069970503653920074d7431e57271c903
davidmiony <57227109+davidmiony@users.noreply.github.com>
2021-01-11 15:05:55 +0100

    aktualizace po dlouhé době (žádné zásadní změny, pouze drobné úpravy)

--- rozpadgraf.py
+++ rozpadgraf.py
@@ -3,6 +3,8 @@
 Created on Thu Dec 26 11:47:58 2019
 
 @author: David
+
+zpracuje daný uložený rozpad a uloží celý graf a přiblížený graf
 """
 
 import numpy as np
@@ -17,47 +19,10 @@ import os.path as op
 
 def rozpadGraf(nazev):
     cas,napeti=np.loadtxt(nazev,delimiter=",")
-    """    
-    trigger=50
-#    cas1,cas2,x,xx=vyhodnoceni(cas,napeti)
-#    print(cas1,cas2)    
-    index1=0
-    for x in napeti:
-        # index1 = ZAČÁTEK PRVNÍHO PULZU
-        if(x>trigger):
-            break
-        index1+=1
-        
-    index1a=index1
-    for x in napeti[index1:]:
-        index1a+=1
-        # index1a = KONEC PRVNÍHO PULZU
-        if(x<trigger):
-            break
-        
-    index2=index1a
-    for x in napeti[index1a:]:
-        # index2 = ZAČÁTEK DRUHÉHO PULZU
-        if(x>trigger):
-            break
-        index2+=1
-        
-    index2a=index2
-    for x in napeti[index2:]:
-        index2a+=1
-        # index2a = KONEC DRUHÉHO PULZU
-        if(x<trigger):
-            break
-        
-    cas1 = caspulsu(cas,napeti,index1,index1a,0.3)
-    cas2 = caspulsu(cas,napeti,index2,index2a,0.3)
     
-    """    
-    
-    
-    cas1,cas2,index1,index2a = vyhodnoceni(cas,napeti)
-    
-    
+    index1,index1a,index2,index2a = vyhodnoceni(cas,napeti)
+    cas1 = caspulsu(cas, napeti, index1, index1a, 0.3)
+    cas2 = caspulsu(cas, napeti, index2, index2a, 0.3)
     
     ROZDIL=float((cas2-cas1)/1e3)
     print("Časová vzdálenost pulsů: {:.2f} us".format(ROZDIL))
@@ -87,4 +52,4 @@ def rozpadGraf(nazev):
     plt.savefig(op.basename(nazev).split(".")[0]+".png")
     plt.close
 
-rozpadGraf("rozpad 30.txt")
+rozpadGraf("rozpad 6.txt")
--- vyhodnoceni.py
+++ vyhodnoceni.py
@@ -6,106 +6,71 @@ Created on Sat Sep 28 21:34:27 2019
 """
 import numpy as np
 
-def caspulsu(cas,napeti,indexZ,indexK,proc):
-    puls = napeti[indexZ:indexK]
-    if(len(puls)==0):
-#        print("BACHA: chybi puls")
-        return -9999e6
-    nap=proc*np.amax(puls)
-    ind=0
-    for x in napeti[indexZ:indexK]:
-        if(x>nap):
-            break
-        ind+=1
-    U1=napeti[indexZ+ind-1]
-    U2=napeti[indexZ+ind]
-    t1=cas[indexZ+ind-1]
-    t2=cas[indexZ+ind]
-    a=(U1-U2)/(t1-t2)
-    b=U1-(a*t1)
-    t=(nap-b)/a
-    return t
-
+def caspulsu(cas,napeti,index,indexa,proc):
+    puls = napeti[index:indexa]
+    if(len(puls)==0 or index >= 2449):
+        cas = -9999e6
+    else:
+        amplitudaindex = np.where(puls==np.amax(puls))[0][0] + index
+        nap = proc*napeti[amplitudaindex]
+        ind = 0
+        for x in puls:
+            ind += 1
+            if(x>nap):
+                break
+    #    if(amplituda1index == index1+1):
+    #        U11 = napeti[index1+ind1-1]
+    #        U21 = napeti[index1+ind1]
+    #        t11 = cas[index1+ind1-1]
+    #        t21 = cas[index1+ind1]
+    #    else:
+        U1 = napeti[amplitudaindex-1]
+        U2 = napeti[amplitudaindex]
+        t1 = cas[amplitudaindex-1]
+        t2 = cas[amplitudaindex]
+        a = (U1-U2)/(t1-t2)
+        b = U1-(a*t1)
+        cas = (nap-b)/a
+    return cas
 def vyhodnoceni(cas, napeti):
-    #indexy
-    
-    trigger=50
-    
+    trigger = 50
+    reset = 20
+    sampleTime = 12*1e3/245
+        
     index1=0
     for x in napeti:
-        # index1 = ZAČÁTEK PRVNÍHO PULZU
-        if(x>trigger):
-    #        index=np.where(x==napeti)
+        if(abs(x)>trigger):
             break
         index1+=1
-    
-    index1a=index1
-    for x in napeti[index1:]:
+    index1 -= 1
+        
+    index1a=index1+1
+    for x in napeti[index1+1:]:
         index1a+=1
-        # index1a = KONEC PRVNÍHO PULZU
-        if(x<trigger):
+        if(x<reset):
             break
     
+    # index1:index1a = kladná (resp. záporná) část prvního pulsu
+    # index1 - jeden prvek před překročení triggeru
+    # index1a - reset (první prvek menší než 20)
+       #  and (abs(x)-abs(napeti[index2]))/sampleTime>1 
     index2=index1a
     for x in napeti[index1a:]:
-        index2+=1
-        # index2 = ZAČÁTEK DRUHÉHO PULZU
-        if(x>trigger):
+        if(abs(x)>trigger and (abs(x)-abs(napeti[index2-1]))/sampleTime>1):
             break
-    
+        index2+=1
+    index2-=1
+        
     index2a=index2
     for x in napeti[index2:]:
         index2a+=1
-        # index2a = KONEC DRUHÉHO PULZU
-        if(x<trigger):
+        if(x<reset):
             break
-    
-#    print("Čas. vzdálenost pulsů: {:.2f} ms".format((caspulsu(index2,index2a,0.3)-caspulsu(index1,index1a,0.3))/1e6))
-    #print("čas 1 puls",caspulsu(index2,index2a,0.3))
-    #print("čas 2 puls",caspulsu(index1,index1a,0.3))
-#    print("Amplituda: {:.2f} mV".format(np.amax(napeti[index1:index1a])))
-#    casamp=np.where(napeti==np.amax(napeti))[0][0]
-#    print("Čas dosažení amplitudy: {:.2f} ms".format(cas[casamp]/1e6))
-    
-    #NALEZENÍ ČASU PULSU
-    
-    """
-    #hodnota=int(input("Napětí U:"))
-    trigger=0.3
-    hodnota=trigger*np.amax(napeti[index1:index1a])
-    index=index1-10
-    for x in napeti[index+1:index1a]:
-        if(x>hodnota):
-            break
-        index+=1
-        #index = HODNOTA PŘED PŘEKROČENÍM HODNOTY NAPĚTÍ
-        
-    U1=napeti[index]
-    U2=napeti[index+1]
-    t1=cas[index-1]
-    t2=cas[index]
-    a=(U1-U2)/(t1-t2)
-    b=U1-(a*t1)
-    t=(hodnota-b)/a
-    """
-
-    casPulsu1 = caspulsu(cas,napeti,index1,index1a,0.3)/1e6
-    casPulsu2 = caspulsu(cas,napeti,index2,index2a,0.3)/1e6
-    return casPulsu1,casPulsu2,index1,index2a
-
-#    print("Začátek pulsu 1: {:.2f} ms".format(casPulsu1))
-#    print("Začátek pulsu 2: {:.2f} ms".format(casPulsu2))
-    
-    #OBSAH PULSŮ
-"""    
-    S1=0
-    S2=0
-    for x in range(index1,index1a):
-        S1+=napeti[x]*(cas[x]-cas[x-1])
-    for x in range(index2,index2a):
-        S2+=napeti[x]*(cas[x]-cas[x-1])
-#    print("Obsah pulsů: {:.2f} uVs, {:.2f} uVs".format(S1*1e-6,S2*1e-6))
-"""    
+    return index1, index1a, index2, index2a
+    #index2a -= 1
+    # index2a bude vždy minimálně o 1 větší, než index2, což může být problém, pokud index2 je poslední sample z napětí (až se tohle vyřeší, teprve potom bude fungovat casPulsu)    
+    #casPulsu1 = caspulsu(cas,napeti,index1,index1a,0.3)/1e6
+    #casPulsu2 = caspulsu(cas,napeti,index2,index2a,0.3)/1e6
 """
 if(__name__=="__main__"):
     puls=np.loadtxt("puls SiPM - kopie.csv",delimiter=";")
