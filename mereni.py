fb7fef8b0979f46b623b7dd53fd29fc0f59d8789
davidmiony <57227109+davidmiony@users.noreply.github.com>
2021-05-21 08:01:23 +0200

    Malé rychlostní optimalizace

--- mereni.py
+++ mereni.py
@@ -19,32 +19,32 @@ casy_mereni.append(0)
 t1 = time.time()
 cislo = 1
 
-while(True):
+while True:
     iter_count += 1
-    if(iter_count % 30 == 0):
+    if iter_count % 30 == 0:
         casy_mereni[-1] = time.time() - t1
         np.savetxt("casyMereni.txt", casy_mereni)
     
-    cas,napeti=naberDataA()
+    cas,napeti = naberDataA()
     napeti = -napeti
     
     pulsy, pulsy_index = vhd.vyhodnoceni(cas,napeti)
             
-    proc=0.3
+    proc = 0.3
     
-    if(len(pulsy) > 1):
-        if(len(pulsy) < 4):
+    if np.size(pulsy) > 1:
+        if np.size(pulsy) < 4:
             cas1 = vhd.caspulsu(cas,napeti,pulsy_index[0],proc)    
             cas2 = vhd.caspulsu(cas,napeti,pulsy_index[1],proc)
         
-            ROZDIL=(cas2-cas1)/1e3
+            ROZDIL = (cas2 - cas1) / 1e3
             #print("Časová vzdálenost pulsů: {:.2f} us".format(ROZDIL))
             
-            print("Čas 1 [ms]:",cas1)
-            print("Čas 2 [ms]:",cas2)
-            print("Rozdíl [ms]:",cas2-cas1)
-            np.savetxt("rozpad {}.txt".format(cislo),(cas,napeti),delimiter=",")
-            cislo+=1
+            print("Čas 1 [ms]:", cas1)
+            print("Čas 2 [ms]:", cas2)
+            print("Rozdíl [ms]:", cas2 - cas1)
+            np.savetxt("rozpad {}.txt".format(cislo), (cas,napeti), delimiter=",")
+            cislo += 1
     #        plt.figure(2)
     #        plt.plot(cas[index1-30:index2a+30],napeti[index1-30:index2a+30])
     #        plt.show()
--- novevyhodnoceni.py
+++ novevyhodnoceni.py
@@ -8,17 +8,17 @@ Created on Sat Jul  4 15:59:43 2020
 import numpy as np
 
 def caspulsu(cas, napeti, puls_index, proc):
-    puls = [abs(napeti[i]) for i in puls_index]
-    amplitudaindex = puls_index[0] + np.where(puls == max(puls))[0][0]
+    puls = [np.absolute(napeti[i]) for i in puls_index]
+    amplitudaindex = puls_index[0] + np.where(puls == np.amax(puls))[0][0]
     nap = napeti[amplitudaindex] * proc
     
-    for i in range(len(puls)):
-        if(puls[i] > nap):
+    for i in range(np.size(puls)):
+        if puls[i] > nap:
             index_napeti = puls_index[0] + i
             
-            U1 = abs(napeti[index_napeti-1])
-            U2 = abs(napeti[index_napeti])
-            if(U1 > U2):
+            U1 = np.absolute(napeti[index_napeti-1])
+            U2 = np.absolute(napeti[index_napeti])
+            if U1 > U2:
                 return -999e6
             t1 = cas[index_napeti-1]
             t2 = cas[index_napeti]
@@ -33,24 +33,26 @@ def vyhodnoceni(cas, napeti):
     pulses, pulses_index, trigger = [], [], 50
         
     puls_index, puls = [], []
-    adding, pulse_expect, reset = False, False, False
-    for i in range(len(napeti)):
-        if(not reset):
-            if(len(pulses) == 1):
-                reset = (abs(napeti[i]) < trigger)
+    adding, pulse_expect, reset, presvih = False, False, False, False
+    presvih_possible = (np.amax(np.absolute(napeti)) > 400)
+    for i in range(np.size(napeti)):
+        if not reset:
+            if np.size(pulses) == 1:
+                reset = (np.absolute(napeti[i]) < trigger)
                 
         pulse_expect_test = False
-        if(pulse_expect and reset):
-            pulse_expect_test = (abs(napeti[i]) > trigger)
+        if pulse_expect and reset:
+            pulse_expect_test = (np.absolute(napeti[i]) > trigger)
         
-        if(napeti[i] > trigger or pulse_expect_test):
-            presvih = (len(pulses) == 1 and max(abs(napeti)) > 400 and cas[i] - cas[pulses_index[0][0]] < 350)
-            if(not presvih):
+        if napeti[i] > trigger or pulse_expect_test:
+            if(presvih_possible):
+                presvih = (np.size(pulses) == 1 and cas[i] - cas[pulses_index[0][0]] < 350)
+            if not presvih:
                 adding = True
                 puls.append(napeti[i])
                 puls_index.append(i)
         else:
-            if(adding):
+            if adding:
                 adding = False
                 pulse_expect = True
                 pulses.append(puls)
