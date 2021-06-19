3ff8baef3570549fa2fe2830c2347e71ca4d4a92
davidmiony <57227109+davidmiony@users.noreply.github.com>
2021-06-19 11:57:34 +0200

    Create channel_B_testing.py

---
+++ channel_B_testing.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 19 11:49:43 2021
+
+@author: David
+"""
+
+from novevyhodnoceni import vyhodnoceni, caspulsu
+from nabiraniB import naberDataB
+import numpy as np
+import matplotlib.pyplot as plt
+
+cas, napeti = naberDataB()
+plt.plot(cas, napeti)
+pulses, pulses_index = vyhodnoceni(cas, napeti)
+print([caspulsu(cas, napeti, i, 0.3) for i in pulses_index])
+
+plt.plot(cas, napeti)
