aae18ec7a44cd397b2ff03ee131d3bace7c0fa5f
davidmiony <57227109+davidmiony@users.noreply.github.com>
2021-07-15 16:12:46 +0200

    Update pulses_saving.py

--- pulses_saving.py
+++ pulses_saving.py
@@ -14,6 +14,7 @@ casy = []
 num = int(input("Počet časů: "))
 nazev = input("Název souboru: ")
 
+count = 0
 while len(casy) < num:
     cas, napeti = naberDataB()
     #cas, napeti = np.loadtxt("2.txt", delimiter=";")
@@ -21,12 +22,15 @@ while len(casy) < num:
     napeti= -napeti
     
     pulses, pulses_index = vyhodnoceni_umelepulsy(cas, napeti)
-    print(pulses)
     casy_ = [caspulsu(cas, napeti, i, 0.3) for i in pulses_index]
     for i in range(len(casy_) - 1):
         casy_rozdil = casy_[i+1] - casy_[i]
         #print(casy_rozdil)
         casy.append(casy_rozdil)
+    print(count)
+    count += 1
+    if count % 50 == 0:
+        np.savetxt(nazev + ".txt", casy, delimiter=";")
 
 np.savetxt(nazev + ".txt", casy, delimiter=";")
 
