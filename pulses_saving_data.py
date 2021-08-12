6105da8d4872b4ef19f1cd41edd3fbb4dc9c4fe5
davidmiony <57227109+davidmiony@users.noreply.github.com>
2021-08-12 12:19:36 +0200

    Create pulses_saving_data.py
    ukládá nejen časy mezi jednotlivými pulsy, ale i všechna data, ve kterých ty pulsy jsou

---
+++ pulses_saving_data.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  3 15:47:34 2021
+
+@author: David
+"""
+from novevyhodnoceni import vyhodnoceni_umelepulsy, caspulsu
+from nabiraniB import naberDataB
+import numpy as np
+#import matplotlib.pyplot as plt
+
+casy = []
+
+num = int(input("Počet časů: "))
+nazev = input("Název souboru: ")
+
+count = 0
+while len(casy) < num:
+    cas, napeti = naberDataB()
+    #cas, napeti = np.loadtxt("2.txt", delimiter=";")
+    
+    napeti= -napeti
+    
+    pulses, pulses_index = vyhodnoceni_umelepulsy(cas, napeti)
+    casy_ = [caspulsu(cas, napeti, i, 0.3) for i in pulses_index]
+    for i in range(len(casy_) - 1):
+        casy_rozdil = casy_[i+1] - casy_[i]
+        #print(casy_rozdil)
+        casy.append(casy_rozdil)
+    print(count)
+    count += 1
+    
+    np.savetxt(nazev + 'data{}'.format(count) + '.txt', (cas, napeti), delimiter=';')
+    
+    if count % 50 == 0:
+        np.savetxt(nazev + ".txt", casy, delimiter=";")
+
+np.savetxt(nazev + ".txt", casy, delimiter=";")
+
+#plt.hist(casy, bins=200)
+#plt.show()
